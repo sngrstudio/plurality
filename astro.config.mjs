1986209ca41b2f43ff0476caf3b24e358880bbcd
radenpioneer <7355835+radenpioneer@users.noreply.github.com>
2024-09-06 14:54:15 +0000

    setup content layer

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,4 +12,7 @@ export default defineConfig({
       enabled: true
     }
   }),
+  experimental: {
+    contentLayer: true
+  }
 });
---
+++ keystatic.config.mjs
@@ -0,0 +1,21 @@
+import {config, fields, singleton} from '@keystatic/core'
+
+export default config({
+    storage: {
+        kind: 'local'
+    },
+
+    singletons: {
+        site: singleton({
+            label: 'Situs',
+            path: './src/data/site/site',
+            format: 'json',
+            schema: {
+                title: fields.text({ label: 'Nama Situs', validation: { isRequired: true } }),
+                description: fields.text({ label: 'Deskripsi Situs', validation: { isRequired: true, length: { max: 160 } } }),
+                logo: fields.image({ label: 'Logo', directory: './src/assets/logo', publicPath: '~/assets/logo', validation: { isRequired: true } }),
+                favicon: fields.image({ label: 'Icon', directory: 'public', publicPath: '', validation: { isRequired: true } })
+            }
+        })
+    }
+})
