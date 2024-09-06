c0c2798dfc0195e87935a82fec7ffea6850da77d
radenpioneer <7355835+radenpioneer@users.noreply.github.com>
2024-09-06 10:20:40 +0000

    setup react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+
+  integrations: [react()]
 });
