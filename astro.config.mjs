6d523504378d1dbf871049389bb24ee5b24755a6
radenpioneer <7355835+radenpioneer@users.noreply.github.com>
2024-09-07 07:51:57 +0000

    setup tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
 import keystatic from '@keystatic/astro'
 import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), ...(process.env.KEYSTATIC ? [keystatic()] : [])],
+  integrations: [
+    react(),
+    tailwind(),
+    ...(process.env.KEYSTATIC ? [keystatic()] : [])
+  ],
   output: 'hybrid',
   adapter: cloudflare({
     platformProxy: {
